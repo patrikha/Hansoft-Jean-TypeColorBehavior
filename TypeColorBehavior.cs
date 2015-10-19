552b8a986ce90250cf6b098accb684651703ea84
U-EUR\patrikha <patrikha@W7SE5CB30316NWL.eur.gad.schneider-electric.com>
2015-10-19 15:03:40 +0200

    Initial commit

---
+++ TypeColorBehavior.cs
@@ -0,0 +1,145 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Xml;
+
+using HPMSdk;
+using Hansoft.ObjectWrapper;
+using Hansoft.ObjectWrapper.CustomColumnValues;
+
+using Hansoft.Jean.Behavior;
+
+namespace Hansoft.Jean.Behavior.TypeColorBehavior
+{
+    public class TypeColorBehavior : AbstractBehavior
+    {
+        public static bool debug = false;
+        string projectName;
+        bool initializationOK = false;
+        string viewName;
+        EHPMReportViewType viewType;
+        List<Project> projects;
+        private List<ProjectView> projectViews;
+        bool inverted = false;
+        string title;
+
+        public TypeColorBehavior(XmlElement configuration)
+            : base(configuration)
+        {
+            projectName = GetParameter("HansoftProject");
+            string invert = GetParameter("InvertedMatch");
+            if (invert != null)
+                inverted = invert.ToLower().Equals("yes");
+            viewName = GetParameter("View");
+            viewType = GetViewType(viewName);
+            title = "TypeColorBehavior: " + configuration.InnerText;
+        }
+
+        public override void Initialize()
+        {
+            projects = new List<Project>();
+            projectViews = new List<ProjectView>();
+            initializationOK = false;
+            projects = HPMUtilities.FindProjects(projectName, inverted);
+            if (projects.Count == 0)
+                throw new ArgumentException("Could not find any matching project:" + projectName);
+            foreach (Project project in projects)
+            {
+                ProjectView projectView;
+                if (viewType == EHPMReportViewType.AgileBacklog)
+                    projectView = project.ProductBacklog;
+                else if (viewType == EHPMReportViewType.AllBugsInProject)
+                    projectView = project.BugTracker;
+                else
+                    projectView = project.Schedule;
+                projectViews.Add(projectView);
+            }
+            initializationOK = true;
+            DoUpdate();
+        }
+
+        public override string Title
+        {
+            get { return title; }
+        }
+
+        // TODO: Subject to refactoting
+        private EHPMReportViewType GetViewType(string viewType)
+        {
+            switch (viewType)
+            {
+                case ("Agile"):
+                    return EHPMReportViewType.AgileMainProject;
+                case ("Scheduled"):
+                    return EHPMReportViewType.ScheduleMainProject;
+                case ("Bugs"):
+                    return EHPMReportViewType.AllBugsInProject;
+                case ("Backlog"):
+                    return EHPMReportViewType.AgileBacklog;
+                default:
+                    throw new ArgumentException("Unsupported View Type: " + viewType);
+            }
+        }
+
+        private void DoUpdate()
+        {
+            if (initializationOK)
+            {
+                foreach (ProjectView view in projectViews)
+                {
+                    foreach (Task child in view.Find(""))
+                    {
+                        var color = GetColor(child);
+                        if (color == null || child.WallItemColor == color)
+                            continue;
+                        child.WallItemColor = (EHPMTaskWallItemColor)color;
+                    }
+                }
+            }
+        }
+
+        // Entry point if custom data is changed
+        public override void OnTaskChangeCustomColumnData(TaskChangeCustomColumnDataEventArgs e)
+        {
+            if (initializationOK)
+            {
+                Task task = Task.GetTask(e.Data.m_TaskID);
+                if (projects.Contains(task.Project) && projectViews.Contains(task.ProjectView))
+                {
+                    var color = GetColor(task);
+                    if (color == null)
+                        return;
+                    task.WallItemColor = (EHPMTaskWallItemColor)color;
+                }
+            }
+        }
+
+        private EHPMTaskWallItemColor? GetColor(Task task)
+        {
+            var type = task.GetCustomColumnValue("Type");
+            if (type == null)
+                return null;
+            var color = TypeColorConfig.GetTypeColor(type.ToString());
+            if (color == null)
+                return EHPMTaskWallItemColor.Orange;
+            switch (color)
+            { 
+                case "Yellow":
+                    return EHPMTaskWallItemColor.Yellow;
+                case "Blue":
+                    return EHPMTaskWallItemColor.Blue;
+                case "Green":
+                    return EHPMTaskWallItemColor.Green;
+                case "Red":
+                    return EHPMTaskWallItemColor.Red;
+                case "Magenta":
+                    return EHPMTaskWallItemColor.Magenta;
+                case "Orange":
+                    return EHPMTaskWallItemColor.Orange;
+            }
+            return null;
+        }
+    }
+}
+
---
+++ TypeColorConfig.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+
+namespace Hansoft.Jean.Behavior.TypeColorBehavior
+{
+    public class TypeColorConfig
+    {
+        private static Dictionary<string, string> typeColors = null;
+
+        public static string GetTypeColor(string type)
+        {
+            type = type.ToLower();
+            if (typeColors == null)
+                TypeColorConfig.ReadConfig();
+
+            if (typeColors.ContainsKey(type))
+                return typeColors[type];
+            return null;
+        }
+
+        public static void ReadConfig()
+        {
+            typeColors = new Dictionary<string, string>();
+            string currentDirectory = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+            FileInfo fInfo = new FileInfo(Path.Combine(currentDirectory, "TypeColors.xml"));
+            if (!fInfo.Exists)
+                throw new ArgumentException("Could not find settings file " + fInfo.FullName);
+            XmlDocument xmlDocument = new XmlDocument();
+            xmlDocument.Load(fInfo.FullName);
+            XmlElement documentElement = xmlDocument.DocumentElement;
+            if (documentElement.Name != "TypeColors")
+                throw new FormatException("The root element of the settings file must be of type TypeColors, got " + documentElement.Name);
+
+            XmlNodeList topNodes = documentElement.ChildNodes;
+
+            foreach (XmlNode node in topNodes)
+            {
+                if (node.NodeType != XmlNodeType.Element)
+                    continue;
+
+                XmlElement el = (XmlElement)node;
+                string type = "";
+                string color = "";
+                foreach (XmlAttribute attr in el.Attributes)
+                {
+                    if (attr.Name.ToLower() == "type")
+                        type = attr.Value.ToLower();
+                    else if (attr.Name.ToLower() == "color")
+                        color = attr.Value;
+                }
+                typeColors.Add(type, color);
+            }
+        }
+    }
+}
